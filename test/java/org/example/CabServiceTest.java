24407e6cb1b0b4f4cc89411d4ecca63b2e0b6edd
rohitdighe19 <rohitdighe47@gmail.com>
2021-08-12 12:50:36 +0530

    Added:add multiple rides fare

--- test/java/org/example/CabServiceTest.java
+++ test/java/org/example/CabServiceTest.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 
 public class CabServiceTest {
 
+    CabService cabService = new CabService();
+
     @Test
     public void testCalculateFare_Should_Return_Total_Fare() {
-
-        CabService cabService = new CabService();
 
         final double distance =260;
         final int minute=90;
@@ -19,8 +19,6 @@ public class CabServiceTest {
     @Test
     public void testCalculateFare_Should_Return_Min_Fare() {
 
-        CabService cabService = new CabService();
-
         final double distance =0.5;
         final int minute=5;
         double totalFare=cabService.calculateFare(distance,minute);
@@ -29,12 +27,20 @@ public class CabServiceTest {
 
     @Test
     public void testCalculateFare_Should_Return_Zero_Fare() {
-
-        CabService cabService = new CabService();
-
         final double distance =0.0;
         final int minute=0;
         double totalFare=cabService.calculateFare(distance,minute);
         assertEquals(0, totalFare,0);
+    }
+
+    @Test
+    public void testCalculateTotalFare_Should_ReturnTotalFare_forMultipleRides() {
+
+        final double distance =2.3;
+        final int minute=7;
+        final int numOfRides=3;
+        double totalFare=cabService.calculateFare(distance,minute);
+        double totalFareForMultipleRide=cabService.totalFareForMultipleRide(totalFare,numOfRides);
+        assertEquals(90, totalFareForMultipleRide,0);
     }
 }
